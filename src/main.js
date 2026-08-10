60da401657f11a0a7f2ebec081334ae55e163502
Zhenyu Zhao <zzha0756@student.monash.edu>
2026-08-10 19:53:11 +1000

    Week3 Activity 5-6 completed

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import './assets/main.css'
-import './style.css'
+// import './style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 // import bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import './style.css'
+
 
 createApp(App).mount('#app')
 
