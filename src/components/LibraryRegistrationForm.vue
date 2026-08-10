baada08e544002b87fc67cb024869fdb5081923c
Zhenyu Zhao <zzha0756@student.monash.edu>
2026-08-10 20:16:16 +1000

    Week3 Activity7 completed

--- src/components/LibraryRegistrationForm.vue
+++ src/components/LibraryRegistrationForm.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container mt-5">
     <div class="row">
-      <div class="col-md-8 offset-md-2">
+      <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2">
         <h1 class="text-center">User Information Form</h1>
 
         <form @submit.prevent="submitForm">
           <div class="row mb-3">
-            <div class="col-md-6">
+            <div class="col-sm-6">
               <label for="username" class="form-label">Username</label>
               <input
                 type="text"
@@ -16,7 +16,7 @@
               >
             </div>
 
-            <div class="col-md-6">
+            <div class="col-sm-6">
               <label for="password" class="form-label">Password</label>
               <input
                 type="password"
@@ -28,7 +28,7 @@
           </div>
 
           <div class="row mb-3">
-            <div class="col-md-6">
+            <div class="col-sm-6">
               <div class="form-check">
                 <input
                   type="checkbox"
@@ -45,7 +45,7 @@
               </div>
             </div>
 
-            <div class="col-md-6">
+            <div class="col-sm-6">
               <label for="gender" class="form-label">Gender</label>
               <select
                 class="form-select"
